887ea2c27d4ab9d4b13caf695a1bbcd42414a579
Stephen <gstephenc@hotmail.com>
2023-07-09 17:53:34 -0400

    put name and status into Map and started HTML

--- ghstatus.js
+++ ghstatus.js
@@ -1,6 +1,16 @@
-const request = require('request');
-request('https://www.githubstatus.com/',  { json: true }, (err, res, body) => {  
-    //console.log(body);
-    console.log(body['components'][0]['name']);
-    console.log(body['components'][0]['status']);
-});
+function CheckStatus(){
+    const statusMap = new Map();
+    const request = require('request');
+    request('https://www.githubstatus.com/',  { json: true }, (err, res, body) => {  
+        //console.log(body);
+        for(const x in body['components']){
+            //console.log(body['components'][x]['name']);
+            //console.log(body['components'][x]['status']);
+            const name = body['components'][x]['name'];
+            const status = body['components'][x]['status'];
+            statusMap.set(name, status);
+        }
+        console.log(statusMap);
+    });
+}
+CheckStatus();
